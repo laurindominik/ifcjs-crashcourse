ea75ade1ab6a69430cca2209d1a8a6067e90c442
Laurin <laurindominik97@gmail.com>
2022-10-02 19:18:10 +0200

    Copy and paste of js script

--- viewer.js
+++ viewer.js
@@ -1,5 +1,6 @@
 import {projects} from './projects.js';
-
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
 
 
 // Get the URL parameter
@@ -19,3 +20,19 @@ const iframe = document.getElementById('model1-iframe');
 //console.log(currentProject.url);
 
 iframe.src = currentProject.url;
+
+
+
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("../../../");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc('./path/TimberStruc');
